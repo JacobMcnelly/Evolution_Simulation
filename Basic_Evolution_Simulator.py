e91b6e958281d1a68f64d99a49d5dcf4546a89c0
JacobMcnelly <151474939+JacobMcnelly@users.noreply.github.com>
2023-11-28 15:22:37 -0500

    Update Basic_Evolution_Simulator.py

--- Basic_Evolution_Simulator.py
+++ Basic_Evolution_Simulator.py
@@ -4,20 +4,20 @@ import random
 ancestor_sequence = []
 bases = ["A","T","C","G"]
 
-# add random base from bases to make random 10 base genome #
+# add randome base from bases to make random 10 base genome #
 for base in range(0,20):
     ancestor_sequence.append(bases[random.randint(0,3)])
 
 # substitution mutation (takes out a base and replaces it with another) #
 def substitution(genome):
-    genome[random.randint(0,len(genome)-1)] = random.choice(bases).lower()
+    genome[random.randint(0,len(genome)-1)] = random.choice(bases)
 
 # deletion mutation (removes a random base from the genome) #
 def deletion(genome):
     genome.pop(random.randrange(len(genome))) 
-    genome.append(random.choice(bases).lower())
+    genome.append(random.choice(bases))
 
-# simulates a generational mutation by picking one of the two types of mutations at random # 
+# simulates a generational mutation by picking one of the two tyoes of mutations at random # 
 def new_gen(generation):
     descendant = generation.copy()
     choice = random.randint(0,1)
@@ -31,7 +31,7 @@ def new_gen(generation):
 generations = int(input("How many generations do you want to simulate?"))
 generations_list = []
 
-# simulates generational mutations across multiple generations and saves the final generation in gen variable #
+# simulates generational mutations accross multiple generations and saves the final generation in gen variable #
 for num in range(0,generations):
     generations_list.append(num)
     # print(generations_list)
@@ -55,5 +55,4 @@ for gen in generations_list:
 # prints ancestor genome and final generation genome in two lists #
 print(f'''First Generation  :{ancestor_sequence}\nFinal Generation1:{gen}''')
 
-print(f'''First Gneration  :{ancestor_sequence}\nFinal Generation1:{gen}''')
 
